ec85fcd8aa1e1fe33b40dbda7a650d0865806830
Malith Sen <senanayake.malith@gmail.com>
2021-06-09 17:32:13 +0530

    Changed the migrations page for homepages table

--- database/migrations/2021_06_05_164426_create_homepages_table.php
+++ database/migrations/2021_06_05_164426_create_homepages_table.php
@@ -31,51 +31,7 @@ class CreateHomepagesTable extends Migration
             $table->text('blog1')->nullable();
             $table->text('blog2')->nullable();
             $table->text('blog3')->nullable();
-            $table->mediumText('tour_itinerary')->nullable();
-            $table->mediumText('introduction')->nullable();
-            $table->text('gallery_image_1')->nullable();
-            $table->text('gallery_image_2')->nullable();
-            $table->text('gallery_image_3')->nullable();
-            $table->text('gallery_image_4')->nullable();
-            $table->text('gallery_image_5')->nullable();
-            $table->text('gallery_image_6')->nullable();
-            $table->text('extra_image_1')->nullable();
-            $table->text('extra_image_2')->nullable();
-            $table->text('extra_image_3')->nullable();
-            $table->text('extra_image_4')->nullable();
-            $table->text('map_url')->nullable();
-            $table->timestamps();
-
-            '' => 'sometimes|mimetypes:video/x-ms-asf,video/x-flv,video/mp4,application/x-mpegURL,video/MP2T,video/3gpp,video/quicktime,video/x-msvideo,video/x-ms-wmv,video/avi',
-            '' => 'nullable',
-            '' => 'nullable',
-            '' => 'nullable',
-            '' => 'nullable',
-            'itinerary2' => 'nullable',
-            'itinerary3' => 'nullable',
-            'itinerary4' => 'nullable',
-            'itinerary5' => 'nullable',
-            'itinerary6' => 'nullable',            
-            'background_img' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'promotional_message_h1' => 'nullable',
-            'promotional_message' => 'nullable',
-            'blog1' => 'nullable',
-            'blog2' => 'nullable',
-            'blog3' => 'nullable',
-            'from_to' => 'nullable',
-            'tour_itinerary' => 'nullable',
-            'introduction' => 'nullable',
-            'gallery_image_1' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'gallery_image_2' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'gallery_image_3' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'gallery_image_4' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'gallery_image_5' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'gallery_image_6' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'extra_image_1' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'extra_image_2' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'extra_image_3' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'extra_image_4' => 'sometimes|mimes:jpg,png,jpeg|max:5048',
-            'map_url' => 'nullable',
+            
         });
     }
 
