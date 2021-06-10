8ea233e093de1ff0908b4538ab3b86696803088f
Malith Sen <senanayake.malith@gmail.com>
2021-06-10 14:39:57 +0530

    authorize deactivated

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -35,9 +35,9 @@ class HomepageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(HomeValidationRequest $request)
     {
-        //
+        dd($request);
     }
 
     /**
