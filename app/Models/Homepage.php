55e3d7cc77f0123ca424b2b766a063ebd04e8592
Malith Sen <senanayake.malith@gmail.com>
2021-06-12 17:28:40 +0530

    Laravel Accessors added to image and video attributes

--- app/Models/Homepage.php
+++ app/Models/Homepage.php
@@ -9,4 +9,20 @@ class Homepage extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function getBackgroundImgAttribute($value)
+    {
+        if (strpos($value, 'https://') !== FALSE || strpos($value, 'http://') !== FALSE) {
+            return $value;
+        }
+        return asset('images/' . $value);
+    }
+
+    public function getVideoAttribute($value)
+    {
+        if (strpos($value, 'https://') !== FALSE || strpos($value, 'http://') !== FALSE) {
+            return $value;
+        }
+        return asset('images/' . $value);
+    }
 }
